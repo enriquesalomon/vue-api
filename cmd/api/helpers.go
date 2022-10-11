c8d98e7087afb7f36127e30e3476bf27593e189d
Enrique Salomon <king92salomon@gmail.com>
2022-10-11 16:57:22 +0800

    Creating a database driver package

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 )
 
+// readJSON tries to read the body of a request and converts it into JSON
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
 	maxBytes := 1048576 // one megabyte
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
@@ -25,6 +26,7 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data in
 	return nil
 }
 
+// writeJSON takes a response status code and aribitrary data and writes a json response to the client
 func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
 	out, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
@@ -45,4 +47,20 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 	}
 
 	return nil
+}
+
+// errorJSON takes an error, and optionally a response status code, and generates and sends
+// a json error response
+func (app *application) errorJSON(w http.ResponseWriter, err error, status ...int) {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	app.writeJSON(w, statusCode, payload)
 }
