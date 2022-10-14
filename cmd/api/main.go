77517d59095154c864119760c681c59b9d05341b
Enrique Salomon <king92salomon@gmail.com>
2022-10-14 13:24:02 +0800

    Seeting up User Model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -31,11 +31,12 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	dsn := "host=localhost port=5432 user=postgres password=password dbname=vueapi sslmode=disable timezone=UTC connect_timeout=5"
+	dsn := os.Getenv("DSN")
 	db, err := driver.ConnectPostgres(dsn)
 	if err != nil {
 		log.Fatal("Cannot connect to database")
 	}
+	defer db.SQL.Close()
 
 	app := &application{
 		config:   cfg,
