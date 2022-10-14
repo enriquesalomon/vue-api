50ff376ab88836554d5fac2fbecc1d9160ac3a30
Enrique Salomon <king92salomon@gmail.com>
2022-10-14 13:58:30 +0800

    Writing the Insert method for the User type

--- internal/data/models.go
+++ internal/data/models.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 const dbTimeout = time.Second * 3
@@ -132,6 +134,8 @@ func (u *User) GetOne(id int) (*User, error) {
 	return &user, nil
 }
 
+// Update updates one user in the database, using the information
+// stored in the receiver u
 func (u *User) Update() error {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -159,6 +163,7 @@ func (u *User) Update() error {
 	return nil
 }
 
+// Delete deletes one user from the datbase, by ID
 func (u *User) Delete() error {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -171,6 +176,36 @@ func (u *User) Delete() error {
 	}
 
 	return nil
+}
+
+// Insert inserts a new user into the datbase, and returns the ID of the newly inserted row
+func (u *User) Insert(user User) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 12)
+	if err != nil {
+		return 0, err
+	}
+
+	var newID int
+	stmt := `insert into users (email, first_name, last_name, password, created_at, updated_at)
+		values ($1, $2, $3, $4, $5, $6) returning id`
+
+	err = db.QueryRowContext(ctx, stmt,
+		user.Email,
+		user.FirstName,
+		user.LastName,
+		hashedPassword,
+		time.Now(),
+		time.Now(),
+	).Scan(&newID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return newID, nil
 }
 
 type Token struct {
