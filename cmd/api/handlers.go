401ccefa7c06f92232cfd676cc8e3e93dc090f18
Enrique Salomon <king92salomon@gmail.com>
2022-10-18 14:42:21 +0800

    Getting a list of all users from the back end API

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"time"
+	"vue-api/internal/data"
 )
 
 // jsonResponse is the type used for generic JSON responses
@@ -100,4 +101,21 @@ func (app *application) Logout(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
+	var users data.User
+	all, err := users.GetAll()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "success",
+		Data:    envelope{"users": all},
+	}
+
+	app.writeJSON(w, http.StatusOK, payload)
 }
